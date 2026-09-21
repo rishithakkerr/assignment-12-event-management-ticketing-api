91f0da4484051682082197a69bd4af316d085ce4
rishithakkerr <2025.rishit@isu.ac.in>
2026-09-22 01:53:13 +0530

    Enhance firebaseConfig to use ENV for credentials
    Refactor Firebase configuration to support environment variable for service account credentials.

--- rishi-thakker-068-assignment-12/config/firebaseConfig.js
+++ rishi-thakker-068-assignment-12/config/firebaseConfig.js
@@ -1,5 +1,20 @@
 const admin = require("firebase-admin");
-const serviceAccount = require("../serviceAccountKey.json");
+
+let serviceAccount;
+
+if (process.env.FIREBASE_SERVICE_ACCOUNT) {
+  serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT);
+  console.log("Firebase key source: ENV | email:", serviceAccount.client_email);
+} else {
+  try {
+    serviceAccount = require("../serviceAccountKey.json");
+    console.log("Firebase key source: FILE | email:", serviceAccount.client_email);
+  } catch (e) {
+    throw new Error(
+      "No Firebase credentials: set FIREBASE_SERVICE_ACCOUNT env var or add serviceAccountKey.json"
+    );
+  }
+}
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),

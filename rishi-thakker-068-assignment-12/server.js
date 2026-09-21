55ddfbcd7668d797542c6ec6480d8507553610ef
rishithakkerr <2025.rishit@isu.ac.in>
2026-09-22 01:49:15 +0530

    Update server.js

--- rishi-thakker-068-assignment-12/server.js
+++ rishi-thakker-068-assignment-12/server.js
@@ -21,7 +21,7 @@ app.use((req, res) => {
   res.status(404).json({ message: "Route not found" });
 });
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
   console.log(`Swagger docs at http://localhost:${PORT}/api-docs`);
